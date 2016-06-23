9cfeafc8bf5567a4cb1d67c8b1af8db4667fe652
Akexorcist <akexorcist@gmail.com>
2016-06-24 00:09:49 +0700

    Add VibrationManager

--- app/src/main/java/com/akexorcist/myapplication/manager/VibrationManager.java
+++ app/src/main/java/com/akexorcist/myapplication/manager/VibrationManager.java
@@ -1,8 +1,15 @@
 package com.akexorcist.myapplication.manager;
 
+import android.content.Context;
+import android.os.Vibrator;
+
 /**
  * Created by Akexorcist on 6/24/2016 AD.
  */
 
 public class VibrationManager {
+    public static void vibrate(Context context) {
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(300);
+    }
 }
