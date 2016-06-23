6dd4d392be4a9411cff27240d162c6830be5bb87
Akexorcist <akexorcist@gmail.com>
2016-06-24 00:09:02 +0700

    Add full backup content

---
+++ app/src/main/java/com/akexorcist/myapplication/manager/VibrationManager.java
@@ -0,0 +1,8 @@
+package com.akexorcist.myapplication.manager;
+
+/**
+ * Created by Akexorcist on 6/24/2016 AD.
+ */
+
+public class VibrationManager {
+}
--- app/src/main/java/com/akexorcist/myapplication/model/MessageItem.java
+++ app/src/main/java/com/akexorcist/myapplication/model/MessageItem.java
@@ -1,34 +1,8 @@
 package com.akexorcist.myapplication.model;
 
 /**
- * Created by Akexorcist on 6/20/2016 AD.
+ * Created by Akexorcist on 6/23/2016 AD.
  */
 
 public class MessageItem {
-    String text;
-    String user;
-
-    public MessageItem() {
-    }
-
-    public MessageItem(String text, String user) {
-        this.text = text;
-        this.user = user;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
 }
