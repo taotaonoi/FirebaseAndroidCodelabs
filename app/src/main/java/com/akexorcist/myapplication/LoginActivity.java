28ced18ece9e15195bec74137bd57b536378c8f0
Akexorcist <akexorcist@gmail.com>
2016-06-25 01:28:21 +0700

    Change public to private

--- app/src/main/java/com/akexorcist/myapplication/LoginActivity.java
+++ app/src/main/java/com/akexorcist/myapplication/LoginActivity.java
@@ -131,7 +131,7 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
         openActivity(ChatRoomActivity.class);
     }
 
-    public boolean isUsernameAndPasswordValidated(String username, String password) {
+    private boolean isUsernameAndPasswordValidated(String username, String password) {
         if (Utility.isUsernameAndPasswordEmpty(username, password)) {
             showBottomMessage(R.string.please_insert_username_password);
             return false;
