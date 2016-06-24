58add405695baf7a23c85cf26056a7f49aea9b35
Akexorcist <akexorcist@gmail.com>
2016-06-25 01:02:54 +0700

    Remove unused code

--- app/src/main/java/com/akexorcist/myapplication/ChatRoomActivity.java
+++ app/src/main/java/com/akexorcist/myapplication/ChatRoomActivity.java
@@ -3,7 +3,6 @@ package com.akexorcist.myapplication;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
